24bef64b80d9ca05d22a9e8569b5296764d2ca1d
xevilmaxx <prova705@gmail.com>
2023-03-13 11:47:05 +0100

    Added Blazor usage sample

---
+++ BlazorQrCodeGen/DTO/Enums/ImageFormats.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorMAP.DTO.Enums
+{
+    public class ImageFormats
+    {
+        public const string Png = "png";
+        public const string Jpeg = "jpeg";
+    }
+}
---
+++ BlazorQrCodeGen/Pages/Index.razor.cs
@@ -0,0 +1,72 @@
+﻿using BlazorDownloadFile;
+using BlazorMAP.DTO.Enums;
+using LibQrGenerator.Enums;
+using LibQrGenerator.Payloads;
+using LibQrGenerator.QrDrawerFactory;
+using Microsoft.AspNetCore.Components;
+using SixLabors.ImageSharp.Formats;
+
+namespace BlazorQrCodeGen.Pages
+{
+    public partial class Index : ComponentBase
+    {
+
+        [Inject]
+        public IBlazorDownloadFileService BlazorDownloadFileService { get; set; }
+
+        private byte[] Img { get; set; }
+        private string Format { get; set; } = ImageFormats.Png;
+
+        private string CustomQrText { get; set; } = "Custom Qr Text";
+
+        private string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var msg = new PayloadGenerator.ContactData(
+                PayloadGenerator.ContactData.ContactOutputType.VCard4,
+                GenerateRandomString(5),
+                GenerateRandomString(10),
+                "EvilMax"
+                ).ToString();
+
+            var qr = new QrDrawerFactory().GetDriver(DrawDriverType.SkiaSharp);
+            Img = qr
+                .SetQr(msg)
+                .SetImageFormat(CommonImageFormat.Png)
+                .GetBytes();
+
+            //var ttt = qr
+            //    .SetQr("Hello Goodbye")
+            //    .SetImageFormat(LibQrGenerator.QrDrawerFactory.CommonImageFormat.Png)
+            //    .Save("./Test.png");
+
+            //StateHasChanged();
+        }
+
+        public async void DownloadQr()
+        {
+
+            var downloadRes = await BlazorDownloadFileService.DownloadFile($"QR_{DateTime.Now.ToString("yyyyMMddHHmmss")}.{Format}", Img, "application/octet-stream");
+
+        }
+
+        public async void GenerateCustomQr()
+        {
+
+            var qr = new QrDrawerFactory().GetDriver(DrawDriverType.ImageSharp);
+            Img = qr
+                .SetQr(CustomQrText)
+                .SetImageFormat(CommonImageFormat.Png)
+                .GetBytes();
+
+        }
+
+    }
+}
